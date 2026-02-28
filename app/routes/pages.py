7fe44625cd1a07143676c6338827775202c1f360
The Eagle <theeaglebenson@gmail.com>
2026-02-28 11:23:01 +0000

    Add homepage, update pages.py, update href attribute in navbar.html and footer.html

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -7,69 +7,39 @@ template = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
-    return template.TemplateResponse(request=request, name="index.html", context={})
+    return template.TemplateResponse(request=request, name="pages/index.html")
 
 @router.get("/youtube", response_class=HTMLResponse)
 async def youtube_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="youtube.html",
-        context={
-            "platform": "youtube",
-            "platform_name": "Youtube",
-            "description": "Download videos and audio from Youtube",
-            "platform_logo_url": "/static/images/platform_logos/youtube.png"
-        }
+        name="youtube.html"
     )
 
 @router.get("/facebook", response_class=HTMLResponse)
 async def facebook_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="facebbok.html",
-        context={
-            "platform": "facebook",
-            "platform_name": "Facebook",
-            "description": "Download videos and reels from Facebook",
-            "platform_logo_url": "/static/images/platform_logos/facebook.png"
-        }
-    )
+        name="facebbok.html"
+        )
 
 @router.get("/instagram", response_class=HTMLResponse)
 async def instagram_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="instagram.html",
-        context={
-            "platform": "instagram",
-            "platform_name": "Instagram",
-            "description": "Download videos and reels from Instagram",
-            "platform_logo_url": "/static/images/platform_logos/instagram.png"
-        }
+        name="instagram.html"
     )
 
 @router.get("/twitter_x", response_class=HTMLResponse)
 async def twitter_x_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="x.html",
-        context={
-            "platform": "x",
-            "platform_name": "X",
-            "description": "Download videos from X",
-            "platform_logo_url": "/static/images/platform_logos/x.png"
-        }
+        name="x.html"
     )
 
 @router.get("/tiktok", response_class=HTMLResponse)
 async def tiktok_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="tiktok.html",
-        context={
-            "platform": "tiktok",
-            "platform_name": "TikTok",
-            "description": "Download videos and audio from TikTok",
-            "platform_logo_url": "/static/images/platform_logos/tiktok.png"
-        }
+        name="tiktok.html"
     )
