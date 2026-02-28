dc46410cf8080cf3e11c81ff4f42d2afca728e27
The Eagle <theeaglebenson@gmail.com>
2026-02-28 11:34:36 +0000

    Add context dictionary in pages.py

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -13,33 +13,63 @@ async def homepage(request: Request):
 async def youtube_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="pages/youtube.html"
+        name="pages/youtube.html",
+        context={
+            "platform": "youtube",
+            "platform_name": "YouTube",
+            "platform_icon": "/static/images/platform_logos/youtube.png",
+            "description": "Download YouTube videos"
+        }
     )
 
 @router.get("/facebook", response_class=HTMLResponse)
 async def facebook_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="pages/facebook.html"
+        name="pages/facebook.html",
+        context={
+            "platform": "facebook",
+            "platform_name": "Facebook",
+            "platform_icon": "/static/images/platform_logos/facebook.png",
+            "description": "Download Facebook videos"
+        }
         )
 
 @router.get("/instagram", response_class=HTMLResponse)
 async def instagram_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="pages/instagram.html"
+        name="pages/instagram.html",
+        context={
+            "platform": "instagram",
+            "platform_name": "Instagram",
+            "platform_icon": "/static/images/platform_logos/instagram.png",
+            "description": "Download Instagram videos"
+        }
     )
 
 @router.get("/twitter_x", response_class=HTMLResponse)
 async def twitter_x_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="pages/twitter_x.html"
+        name="pages/twitter_x.html",
+        context={
+            "platform": "twitter_x",
+            "platform_name": "X",
+            "platform_icon": "/static/images/platform_logos/x.png",
+            "description": "Download videos from tweets"
+        }
     )
 
 @router.get("/tiktok", response_class=HTMLResponse)
 async def tiktok_page(request: Request):
     return template.TemplateResponse(
         request=request,
-        name="pages/tiktok.html"
+        name="pages/tiktok.html",
+        context={
+            "platform": "tiktok",
+            "platform_name": "TikTok",
+            "platform_icon": "/static/images/platform_logos/tiktok.png",
+            "description": "Download TikTok videos"
+        }
     )
